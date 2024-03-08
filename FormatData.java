e4b8553b5b0424dd7e5f3c1cfd2bb585e8b009bf
Gabriel Savard <123707889+Sp0k@users.noreply.github.com>
2024-03-08 16:45:17 -0400

    Added outputs for sql files and easier usage

--- FormatData.java
+++ FormatData.java
@@ -31,24 +31,32 @@ public class FormatData {
     // Scanner
     Scanner in = new Scanner(System.in);
 
+    // Constants for easy use
+    final String filepathCsv = "path/to/your/csv/"; // Inputs
+    final String filepathSql = "path/to/your/sql/"; // Outputs
+
     // Main Variables
+    System.out.print("Enter the name of the Database to use: ");
+    String database = in.nextLine();
     System.out.print("Enter the number of files to format: ");
     int n = in.nextInt();
     in.nextLine();
+    System.out.println();
 
     // Processing
     // Formatting Data
     for (int i = 0; i < n; i++) {
       // Loop variables
-      System.out.print("Enter the name of the file: ");
+      System.out.print(String.format("Enter the name of the file #%d: ", (i + 1)));
       String filename = in.nextLine();
       System.out.print("Enter the name of the table: ");
       String table_name = in.nextLine();
-      String filepath_in = String.format("data_juliette/csv/%s.csv", filename);
-      String filepath_out = String.format("data_juliette/txt/%s.txt", filename);
+      String filepath_in = String.format("%s%s.csv", filepathCsv, filename);
+      String filepath_out = String.format("%s%s.sql", filepathSql, filename);
       String file = myManager.readFile(filepath_in);
       String fileData[] = file.split("\n");
-      StringBuilder fileContent = new StringBuilder(String.format("INSERT INTO %s;\nVALUES\n", table_name));
+      StringBuilder fileContent = new StringBuilder(
+          String.format("USE %s;\nINSERT INTO %s;\nVALUES\n", database, table_name));
 
       for (int j = 1; j < fileData.length; j++) {
         String tmp[] = fileData[j].split(",");
