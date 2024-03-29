2af62b70b0b5f17b85fb2db95bdbe0d13c7a5c22
Gabriel Savard <123707889+Sp0k@users.noreply.github.com>
2024-03-28 22:45:19 -0300

    Updated and commented FormatData.java

--- FormatData.java
+++ FormatData.java
@@ -1,39 +1,77 @@
+/*
+ *    █████████   █████████
+ *   ███░░░░░███ ███░░░░░███        Format Data
+ *  ███     ░░░ ░███    ░░░
+ * ░███         ░░█████████         @author Gabriel Savard
+ * ░███    █████ ░░░░░░░░███        @version 1.3
+ * ░░███  ░░███  ███    ░███        since 2024-03-08
+ *  ░░█████████ ░░█████████
+ *   ░░░░░░░░░   ░░░░░░░░░
+ *
+ *  @description: The program will go through any amount of csv files
+ *  and output one sql file per csv file. It can tell apart a String
+ *  from a numerical value.
+ *
+ *  @requisites: GSFileManager
+ */
+
 import java.util.Scanner;
 import GSFileManager.FileManager;
 
 public class FormatData {
+  /* FILE PATHS TO MODIFY BEFORE USE */
+  // Input
+  final static String filepathCsv = "PATH/TO/YOUR/CSV/FILES/"; // The '/' is required to work properly
+
+  // Output
+  final static String filepathSql = "PATH/TO/YOUR/SQL/FILES/";
+
+  /*
+   * Checks if the value requires to be between apostrophees
+   *
+   * @param str A String to be checked
+   * 
+   * @return boolean A boolean value
+   */
   static boolean isNum(String str) {
     // method variables
     char[] nums = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '-' };
     boolean isNum;
+    int dashCounter = 0;
 
-    for (int i = 0; i < str.length(); i++) {
-      isNum = false;
+    // Processing
+    if (!str.equalsIgnoreCase("NULL")) {
+      for (int i = 0; i < str.length(); i++) {
+        isNum = false;
 
-      for (int j = 0; j < nums.length; j++) {
-        if (str.charAt(i) == nums[j]) {
-          isNum = true;
-          break;
+        // Check if the String contains numbers
+        for (int j = 0; j < nums.length; j++) {
+          if (str.charAt(i) == nums[j]) {
+            if (str.charAt(i) == '-')
+              dashCounter++;
+            isNum = true;
+            break;
+          }
         }
-      }
 
-      if (!isNum)
-        return false;
+        // Current character is a letter, value is a String
+        if (!isNum || dashCounter >= 2)
+          return false;
+      }
     }
 
     return true;
   }
 
+  /*
+   * Main method
+   */
   public static void main(String[] args) {
     // File manager
     FileManager myManager = new FileManager();
 
     // Scanner
     Scanner in = new Scanner(System.in);
-
-    // Constants for easy use
-    final String filepathCsv = "path/to/your/csv/"; // Inputs
-    final String filepathSql = "path/to/your/sql/"; // Outputs
 
     // Main Variables
     System.out.print("Enter the name of the Database to use: ");
@@ -58,21 +96,31 @@ public class FormatData {
       StringBuilder fileContent = new StringBuilder(
           String.format("USE %s;\nINSERT INTO %s;\nVALUES\n", database, table_name));
 
+      // Constructing the lines to be added
       for (int j = 1; j < fileData.length; j++) {
+        // Separate the values and start the line
         String tmp[] = fileData[j].split(",");
         String out = "(";
+
         for (int k = 0; k < tmp.length; k++) {
-          if (!isNum(tmp[k]))
+          // Check if the value is a String, then adds them properly
+          if (!isNum(tmp[k])) {
+            // Format the Strings to be properly used
+            if (tmp[k].contains("\'"))
+              tmp[k] = tmp[k].replace("\'", "\'\'");
+
             out += String.format("\'%s\'", tmp[k]);
-          else
+          } else
             out += tmp[k];
 
+          // Verify if it's the end of the line or not
           if (k < tmp.length - 1)
             out += ',';
           else
             out += ')';
         }
 
+        // Verify if it's the end of the file or not
         if (j < fileData.length - 1)
           out += ",\n";
         else
@@ -90,6 +138,6 @@ public class FormatData {
 
     // Finish program
     in.close();
-    System.out.println("Done\n");
+    System.out.println("Done!");
   }
 }
